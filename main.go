fda4dfb3561e875f23be12979bea7782482eaa68
hyperbel <thomaszimmer524@gmail.com>
2022-11-09 17:53:40 +0100

    get home dir and cmd args

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"os"
+	"fmt"
+	"log"
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	args := os.Args
+	if args == nil {
+		log.Fatal("no args provided")
+	}
+	fmt.Println(args)
+
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(dir)
+	
 	r := gin.Default()
 
 	r.GET("/", index)
